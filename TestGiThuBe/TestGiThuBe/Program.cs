dc48cd1b83770d72a50e5ffc96a821a0823a6be9
JohanHarboe <Johanharboe20@gmail.com>
2014-09-27 16:28:38 +0200

    Godt I Gang Test

--- TestGiThuBe/TestGiThuBe/Program.cs
+++ TestGiThuBe/TestGiThuBe/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,11 @@ namespace TestGiThuBe
 {
     class Program
     {
-        private static readonly int[] Data = { 3, 4, 34, 22, 34, 3, 4, 4, 2, 7, 3, 8, 122, -1, -3, 1, 2 };
+        private static int[] Data = { 3, 4, 34, 22, 34, 3, 4, 4, 2, 7, 3, 8, 122, -1, -3, 1, 2 };
+
+        private static List<int> _list = new List<int>(Data);
+ 
+        
 
         private static int FindSmallest(IList<int> numbers)
         {
@@ -26,19 +31,35 @@ namespace TestGiThuBe
             }
             //Console.WriteLine(String.Join("; ", numbers) + ": " + smallestSoFar);
             return smallestSoFar;
+            List<int> list = Data.ToList();
+            
         }
 
      
 
         static void Main(string[] args)
         {
+            //var list = new List<int>(Data);
              // 1. partition -->
+            int smallest = FindSmallest(_list);
+            //<--
+            Console.WriteLine(smallest);
+            foreach (var num in _list)
+            {
+                Console.WriteLine(num);
+            }
+
+            ConcurrentBag<int> cbag = new ConcurrentBag<int>(_list);
+            int value;
+            cbag.TryPeek(out value); //Check value
+            Console.WriteLine("TryPeek: {0}", value); //Viser det sidste der blev puttet i.
+            Console.WriteLine("Counter: {0}", cbag.Count); //Viser antal elementer i bag.
+            // Console.WriteLine(_list);
 
 
 
-
-       //2. for each partition find the smallest element and add to ConcurrentBag
-       //3. find the smallest element in the ConcurrentBag
+            //2. for each partition find the smallest element and add to ConcurrentBag
+            //3. find the smallest element in the ConcurrentBag
         }
     }
 }
